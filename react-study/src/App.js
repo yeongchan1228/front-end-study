a51932741511ad4115871febe9ee1a59228abf0f
최영찬 <gks54410@gmail.com>
2022-08-07 13:32:05 +0900

    Feat: cleanup function 적용

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,42 +1,26 @@
-// import Button from "./Button";
-// import styles from "./App.module.css";
 import { useEffect, useState } from "react";
 
+function Hello() {
+  function destroyedFn() {
+    console.log("destroyed.");
+  }
+  function effectFn() {
+    console.log("created.");
+    return destroyedFn;
+  }
+  useEffect(effectFn, []);
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => {
-    setKeyword(event.target.value);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => {
+    setShowing((prev) => !prev);
   };
-
-  console.log("I run all the time.");
-
-  const iRunOnlyOne = () => {
-    console.log("I run only once.");
-  };
-
-  useEffect(iRunOnlyOne, []); // 딱 한 번만
-  useEffect(() => {
-    // keyword에 변화가 발생했을 때
-    if (keyword !== "" && keyword.length > 5)
-      console.log(
-        "I run when 'keyword' changes and keyword not null and keyword > 5"
-      );
-  }, [keyword]);
-  useEffect(() => console.log("I run when 'counter' changes"), [counter]);
-  useEffect(() => console.log("I run when keyword & counter changes"));
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me!</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "show"}</button>
     </div>
   );
 }
