e4bc217df8b7e3c20d4d67a356893edace23816b
최영찬 <gks54410@gmail.com>
2022-08-07 14:09:31 +0900

    Feat: Todo List 만들기

---
+++ react-study/src/Todos.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+function Todos() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => {
+    setToDo(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  console.log(toDo);
+  console.log(toDos);
+  return (
+    <div>
+      <h1>My ToDos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          onChange={onChange}
+          type="text"
+          placeholder="Write your to do..."
+        ></input>
+        <button>Add ToDo</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Todos;
