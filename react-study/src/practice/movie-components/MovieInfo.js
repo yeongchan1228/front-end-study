1a681a3b1f21b74ea0b73e64b4b25da69ec7d8a6
최영찬 <gks54410@gmail.com>
2022-08-07 17:57:50 +0900

    Feat: css 추가

--- react-study/src/practice/movie-components/MovieInfo.js
+++ react-study/src/practice/movie-components/MovieInfo.js
@@ -1,14 +1,20 @@
+import styles from "./Component.module.css";
+import styles2 from "./Movie.module.css";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
 function MovieInfo({ movie }) {
   return (
-    <div key={movie.id}>
+    <div className={styles2.movie} key={movie.id}>
       <img src={movie.medium_cover_image} alt="profile"></img>
       <h2>
         <Link to={`/movie/${movie.id}`}>{movie.title}</Link>
       </h2>
-      <p>{movie.summary}</p>
+      <p>
+        {movie.summary.length > 235
+          ? `${movie.summary.slice(0, 235)}...`
+          : movie.summary}
+      </p>
       <ul>
         {movie.genres.map((genre, index) => (
           <li key={index}>{genre}</li>
