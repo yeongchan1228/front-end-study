a06b12fec112d754493764445e5bfa18c3aa71e4
최영찬 <gks54410@gmail.com>
2022-08-07 12:12:20 +0900

    Feat: css 적용

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,7 +1,12 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+
 function App() {
   return (
     <div>
-      <h1>Welcome back to react!</h1>
+      <h1 className={styles.title}>Welcome back to react!</h1>
+
+      <Button text="button" />
     </div>
   );
 }
